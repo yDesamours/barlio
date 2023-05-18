0b13e72f927c182859b05eea5640ff4911beff8a
unknown <yveltamours@gmail.com>
2023-05-18 13:46:36 -0400

    model article

--- cmd/server/app.go
+++ cmd/server/app.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"barlio/cmd/models"
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -14,8 +16,10 @@ type App struct {
 	Server         *http.Server
 	SessionManager *scs.SessionManager
 	DB             *sql.DB
-	Logger         Logger
+	Logger         *Logger
 	Config         Config
+	User           *models.UserModel
+	Article        *models.ArticleModel
 }
 
 type Config struct {
@@ -31,19 +35,41 @@ func newApp(conf *Config) *App {
 	sessionManager := scs.New()
 	sessionManager.Lifetime = time.Hour
 
-	server := &http.Server{
+	server := http.Server{
 		Addr: fmt.Sprintf(":%d", conf.Server.Port),
 	}
 
+	db, err := newDB(conf.DB.DSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := &App{
+		DB:             db,
 		Config:         *conf,
-		Server:         server,
+		Server:         &server,
 		SessionManager: sessionManager,
-		Logger:         *newLogger(os.Stdout),
+		Logger:         newLogger(os.Stdout),
+		User:           &models.UserModel{DB: db},
+		Article:        &models.ArticleModel{DB: db},
 	}
 	app.routes()
 
 	return app
+}
+
+func newDB(dsn string) (*sql.DB, error) {
+	DB, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return DB, nil
 }
 
 func (app *App) ListenAndServe() error {
