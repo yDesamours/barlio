3d79a66b26cf39e19510b010060e9fdb01d3b1fa
unknown <yveltamours@gmail.com>
2023-06-16 22:24:57 -0400

    token model functions

--- cmd/server/model/token.go
+++ cmd/server/model/token.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
@@ -22,13 +23,45 @@ type TokenModel struct {
 }
 
 func (t *TokenModel) Insert(token *Token) error {
+	const statement = `INSERT INTO tokens
+							(userid, scope, hash, expire_at)
+						VALUES($1, $2, $3, $4)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, statement, token.Userid, token.Scope, token.Hash, token.ExpiretAt)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (t *TokenModel) DeleteExpired() error {
+	const statement = `DELETE FROM tokens 
+						WHERE expired_at < $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, statement, time.Now().Format("2006-01-02"))
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (t *TokenModel) DeleteForUser(userId int, scope string) error {
+	const statement = `DELETE FROM tokens 
+						WHERE userid=$1 AND scope=$2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, statement, userId, scope)
+	if err != nil {
+		return err
+	}
 	return nil
 }
